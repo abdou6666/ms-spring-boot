c77bccf3574ed49176d2b2091ad3334c8482ac4e
abdou6666 <abdousfayhi12@gmail.com>
2023-10-16 13:02:13 +0100

    configured cricuit breaker pattern with resilience4j

--- microservices-new/inventory-service/src/main/java/com/abdou/inventoryservice/service/InventoryService.java
+++ microservices-new/inventory-service/src/main/java/com/abdou/inventoryservice/service/InventoryService.java
@@ -3,6 +3,8 @@ package com.abdou.inventoryservice.service;
 import com.abdou.inventoryservice.dto.InventoryResponse;
 import com.abdou.inventoryservice.repository.InventoryRepository;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,10 +13,15 @@ import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
     @Transactional(readOnly = true)
-    public List<InventoryResponse> isInStock(List<String> skuCode) {
+    @SneakyThrows
+    public List<InventoryResponse> isInStock(List<String> skuCode)  {
+        log.info("Wait started");
+        Thread.sleep(1000);
+        log.info("Wait ended");
         return inventoryRepository.findBySkuCodeIn(skuCode)
                 .stream()
                 .map(
